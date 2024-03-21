244074f377709bff999790d15626ae4822495c51
V ANIL KUMAR <vanilkumar4164@gmail.com>
2024-03-22 00:35:36 +0530

    Add files via upload

---
+++ sho.js
@@ -0,0 +1,6 @@
+let getText = document.getElementById("paraText");
+let buttonEl = document.getElementById("buttonEl");
+buttonEl.onclick=function(){
+    getText.textContent="Rs 900 saved sucessfully";
+}
+
